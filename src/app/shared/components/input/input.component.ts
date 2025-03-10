4461d5b2f5a36873a3f3796ccde510608fdd2984
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2025-03-10 10:35:19 -0300

    refactor: input component

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -1,11 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  forwardRef,
+} from '@angular/core';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 export type HTMLInputTypeAttribute = 'text' | 'email' | 'number' | 'tel';
 
 @Component({
   selector: 'dev-avonale-input',
   standalone: true,
-  imports: [],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
+  imports: [FormsModule],
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
 })
@@ -15,8 +29,28 @@ export class InputComponent {
   @Input() type: HTMLInputTypeAttribute = 'text';
   @Output() valueChange = new EventEmitter<string>();
 
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
   onInputChange(event: Event) {
     const inputValue = (event.target as HTMLInputElement).value;
+    this.value = inputValue;
+    this.onChange(inputValue);
+    this.onTouched();
     this.valueChange.emit(inputValue);
+  }
+
+  writeValue(value: string): void {
+    if (value !== undefined) {
+      this.value = value;
+    }
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
   }
 }
